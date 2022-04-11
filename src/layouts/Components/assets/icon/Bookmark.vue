baef5f9f9157fa2e26faf3b5a9003fbcf6ae48b8
Siamak.Ahmadi <mrsiamak.dev@gmail.com>
2022-04-11 14:24:23 +0430

    Some Change in Left Sidabar Icons

---
+++ src/layouts/Components/assets/icon/Bookmark.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9.25 9.05C11.03 9.7 12.97 9.7 14.75 9.05"
+      stroke="#656565"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M16.82 2H7.18001C5.05001 2 3.32001 3.74 3.32001 5.86V19.95C3.32001 21.75 4.61001 22.51 6.19001 21.64L11.07 18.93C11.59 18.64 12.43 18.64 12.94 18.93L17.82 21.64C19.4 22.52 20.69 21.76 20.69 19.95V5.86C20.68 3.74 18.95 2 16.82 2Z"
+      stroke="#656565"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M16.82 2H7.18001C5.05001 2 3.32001 3.74 3.32001 5.86V19.95C3.32001 21.75 4.61001 22.51 6.19001 21.64L11.07 18.93C11.59 18.64 12.43 18.64 12.94 18.93L17.82 21.64C19.4 22.52 20.69 21.76 20.69 19.95V5.86C20.68 3.74 18.95 2 16.82 2Z"
+      stroke="#656565"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
---
+++ src/layouts/Components/assets/icon/More.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y="10" width="5" height="5" rx="2.5" fill="#656565" />
+    <rect x="9.21051" y="10" width="5" height="5" rx="2.5" fill="#656565" />
+    <rect x="18.4211" y="10" width="5" height="5" rx="2.5" fill="#656565" />
+  </svg>
+</template>

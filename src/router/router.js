e88e3f9cb9d2a577a54b36575146e0237ed2c54f
Siamak.Ahmadi <mrsiamak.dev@gmail.com>
2022-04-14 01:15:29 +0430

    add notification component

--- src/router/router.js
+++ src/router/router.js
@@ -13,7 +13,7 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/home',
+            path: '/',
             name: 'home',
             component: Home,
             meta: { layout: 'Home' }

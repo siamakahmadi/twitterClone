c8217a6c49bd88b1685fce4d4fca88e56ed2a93b
Siamak.Ahmadi <mrsiamak.dev@gmail.com>
2022-04-17 23:47:45 +0430

    Complete Direct

--- src/components/DirectRow/index.vue
+++ src/components/DirectRow/index.vue
@@ -1,24 +1,26 @@
 <template>
-  <div :class="isSelect ? 'direct-row select' : 'direct-row'">
+  <div :class="isActive ? 'direct-row select' : 'direct-row'">
     <div class="direct">
       <div class="user">
         <div class="profile">
-          <img src="../../assets/images/profile1.jpg" alt="" />
+          <img :src="data.profileUrl" alt="" />
         </div>
 
         <div class="info">
           <div class="user-info">
-            <div class="username"><p class="text">Siamak Ahmadi</p></div>
+            <div class="username">
+              <p class="text">{{ data.name }}</p>
+            </div>
             <div class="userid"><p class="text">@_siamakahmadi</p></div>
           </div>
           <div class="message-summary">
-            <p class="text">See That , The Best tweet the same freamwork ...</p>
+            <p class="text">{{data.messageSummary}}</p>
           </div>
         </div>
       </div>
 
       <div class="toolbar">
-        <div class="time"><p class="text">17h</p></div>
+        <div class="time"><p class="text">{{data.messageTime}}</p></div>
         <div class="more-icon">
           <img src="./icon/More.svg" alt="" />
         </div>
@@ -27,15 +29,11 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    isSelect: {
-      type: Boolean,
-      default: true,
-    },
-  },
-};
+<script setup>
+defineProps({
+  isActive: Boolean,
+  data: Object,
+});
 </script>
 
 <style lang="scss">
--- src/views/Message/index.vue
+++ src/views/Message/index.vue
@@ -5,24 +5,79 @@
       <template v-slot:icon> icon </template>
     </PageTitle>
 
-    <DirectRow :isSelect="true"/>  
-
-
-
-
-
+    <DirectRow
+      v-for="direct in directList"
+      :data="direct"
+      :key="direct.id"
+      :isActive="false"
+    />
   </div>
 </template>
 
-<script>
-import DirectRow from '../../components/DirectRow/index.vue'
-export default {
-  components:{
-    DirectRow
-  }
-};
+<script setup>
+import DirectRow from "../../components/DirectRow/index.vue";
+import { ref } from "vue";
+
+const directList = ref([
+  {
+    id: 1,
+    name: "SiamakAhmad",
+    messageSummary: "i have go to the gym",
+    messageTime: "25m",
+    profileUrl:
+      "https://pbs.twimg.com/profile_images/1409956076171190280/KrOnjrOU_400x400.jpg",
+  },
+  {
+    id: 2,
+    name: "Sina Ahmadi",
+    messageSummary: "are you ok?",
+    messageTime: "1h",
+    profileUrl:
+      "https://pbs.twimg.com/profile_images/1510983363347333121/qNb_N_F-_400x400.jpg",
+  },
+  {
+    id: 3,
+    name: "I Hani",
+    messageSummary: "watsupp?",
+    messageTime: "2h ago",
+    profileUrl:
+      "https://pbs.twimg.com/profile_images/1490601661382934530/e1-TBLWF_400x400.jpg",
+  },
+  {
+    id: 4,
+    name: "Alireza Kargar",
+    messageSummary: "i riding to your house",
+    messageTime: "1h ",
+    profileUrl:
+      "https://pbs.twimg.com/profile_images/1509650694319620102/BoZ08g1u_400x400.jpg",
+  },
+  {
+    id: 5,
+    name: "Mojtaba Mansori",
+    messageSummary: "you are the best player",
+    messageTime: "1h",
+    profileUrl:
+      "https://pbs.twimg.com/profile_images/1508711484758626308/-tJD8n_i_400x400.jpg",
+  },
+  {
+    id: 6,
+    name: "Mohammad M",
+    messageSummary: "this is amazing",
+    messageTime: "13h",
+    profileUrl:
+      "https://pbs.twimg.com/profile_images/1454580164428541954/-_FLLs6k_400x400.jpg",
+  },
+  {
+    id: 7,
+    name: "Morteza Ahmadi",
+    messageSummary: "i have go to the gym",
+    messageTime: "32h",
+    profileUrl:
+      "https://pbs.twimg.com/profile_images/1449140119735357443/2loXWe3w_400x400.jpg",
+  },
+]);
 </script>
 
 <style lang="scss">
-@import './style.scss';
+@import "./style.scss";
 </style>
